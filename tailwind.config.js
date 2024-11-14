9e9f81e762da2e3db478dcdb92d7d795217fc95c
joo_manasseh <52069849+Joo-Wee@users.noreply.github.com>
2024-11-14 19:58:34 +0000

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'header-color': '#2E1A47',
+        'header-color_btn': '#6e57e0',
+        'deep-purple': '#2E1A47',
+        'turqoise': '#40E0D0',
+        'body-color': '#9e96eb'
+    },
+
+      backgroundImage: {
+        'hero-pattern': "url('/src/media/purple_dark_blue_turquoise_blue_square_shapes_art_pattern_4k_hd_abstract.png')",
+        'games-pattern': "url('/src/media/OBSKLD0.jpg')"
+      }
+  },
+  plugins: [],
+}
+}
